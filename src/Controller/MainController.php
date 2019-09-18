e1fe0611b92a524512794d51922db2373208f541
ErwanSap <erwan.sapin2019@campus-eni.fr>
2019-09-18 16:02:44 +0200

    ajout de tous les composers requi pour le développememnt de l'application.

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/", name="home")
+     */
+    public function home(){
+
+        return $this -> render('main/home.html.twig');
+    }
+
+}
